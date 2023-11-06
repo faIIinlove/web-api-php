3fd4739edb8e67c10853e07167254bee808df888
faIIinlove <phucnamvan@gmail.com>
2023-11-06 23:13:03 +0700

    Add new columns to users table and insert new data

--- database/db.sql
+++ database/db.sql
@@ -2,16 +2,19 @@ CREATE DATABASE IF NOT EXISTS MD18101;
 USE MD18101;
 
 -- TẠO BẢNG
+-- tạo bảng users {id, email, password, name, phone, gender, student_code, picture, address, role}
 CREATE TABLE IF NOT EXISTS `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `name` varchar(255) NOT NULL,
   `phone` varchar(255) NOT NULL,
+  `gender` varchar(255) NOT NULL,
+  `student_code` varchar(255) NOT NULL,
+  `picture` varchar(255) NOT NULL,
   `address` varchar(255) NOT NULL,
   `role` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `email` (`email`)
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- tạo bảng topics (id, name, description)
@@ -41,9 +44,13 @@ CREATE TABLE IF NOT EXISTS `news` (
 
 
 -- thêm dữ liệu vào bảng users
-INSERT INTO `users` (`id`, `email`, `password`, `name`, `phone`, `address`, `role`) VALUES
-(1, 'abc@gmail.com', '123456', 'Nguyễn Văn A', '0123456789', 'Hà Nội', 'admin'),
-(2, 'admin@gmail.com', '123456', 'Nguyễn Văn B', '0123456789', 'Hà Nội', 'admin');
+INSERT INTO `users` (`id`, `email`, `password`, `name`, `phone`, `gender`, `student_code`, `picture`, `address`, `role`) VALUES
+(1, 'phongphi@gmail.com', '123456', 'Phong Phi', '0123456789', 'Nam', 'PH123456', 'https://firebasestorage.googleapis.com/v0/b/aniflix-958d2.appspot.com/o/%E1%BA%A2nh%2FPhi.jpg?alt=media&token=b2255ba9-7712-41cb-8cd9-eb709736ed10', 'Hà Nội', 'user'),
+(2, 'anhduy@gmail.com', '123456', 'Anh Duy', '0123456789', 'Nam', 'PH123456', 'https://firebasestorage.googleapis.com/v0/b/aniflix-958d2.appspot.com/o/%E1%BA%A2nh%2Fanh.jpg?alt=media&token=1bb0f3bd-c49b-4cb5-a55e-fade4f027b68', 'Hà Nội', 'user'),
+(3, 'cuphuc@gmail.com', '123456', 'Cụ Phúc', '0123456789', 'Nam', 'PH123456', 'https://firebasestorage.googleapis.com/v0/b/aniflix-958d2.appspot.com/o/%E1%BA%A2nh%2Fc%E1%BB%A5.jpg?alt=media&token=49266b7b-0277-47ed-822d-26e4810111de', 'Hà Nội', 'user'),
+(4, 'qui@gmail.com', '123456', 'Quí', '0123456789', 'Nam', 'PH123456', 'https://firebasestorage.googleapis.com/v0/b/aniflix-958d2.appspot.com/o/%E1%BA%A2nh%2Fdaubuoi.jpg?alt=media&token=0cfcb1d6-34a6-40e6-9f69-45c3f6e7798c', 'Hà Nội', 'user'),
+(5, 'phap@gmail.com', '123456', 'Pháp', '0123456789', 'Nam', 'PH123456', 'https://firebasestorage.googleapis.com/v0/b/aniflix-958d2.appspot.com/o/%E1%BA%A2nh%2Fngu.jpg?alt=media&token=ba030c9a-99cd-4169-8f9a-f728669551ab', 'Hà Nội', 'user'),
+(6, 'phucnamvan123@gmail.com', '123456', 'Văn Nam Phúc', '0123456789', 'Nam', 'PS25452', 'https://lh3.googleusercontent.com/ogwAKPQZvx53PP6RhecpAqg5Ig9Y79L6mg3-LduaWLlGctWSw=s720-c-mo', 'Hà Nội', 'admin');
 
 -- thêm dữ liệu vào bảng topics
 INSERT INTO `topics` (`id`, `name`, `description`) VALUES
