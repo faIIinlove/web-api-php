88260b03530c59861b6962c9d6060f0fa82c8763
faIIinlove <phucnamvan@gmail.com>
2023-11-06 22:51:05 +0700

    Add new PHP files for user registration, forgot password, news management, and database connection.

--- connection/connection.php
+++ connection/connection.php
@@ -8,6 +8,8 @@ try {
 	$dbConn = new PDO("mysql:host={$databaseHost};dbname={$databaseName}", 
 						$databaseUsername, $databasePassword);
 	$dbConn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	echo "Kết nối thành công";
 } catch (PDOException $e) {
 	echo $e->getMessage();
 }
+?>
