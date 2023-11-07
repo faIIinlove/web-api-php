2c950f9b54f758eb630ad7be1db0bc9bbadabbb1
faIIinlove <phucnamvan@gmail.com>
2023-11-07 21:31:05 +0700

    Add OTP functionality for password change and forgot password

---
+++ change-password-with-otp.php
@@ -0,0 +1,55 @@
+<?php
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST"); // You're handling a POST request, not GET
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+include_once 'connection.php';
+$data = json_decode(file_get_contents('php://input'));
+
+$email = $data->email;
+$otp = $data->otp;
+$newPassword = $data->newPassword;
+$confirmPassword = $data->confirmPassword;
+
+//get user id
+$stmt = $dbConn->prepare("SELECT id FROM users WHERE email = ?");
+$stmt->execute([$email]);
+$user_id = $stmt->fetchColumn();
+
+// Get hashed OTP from database
+$stmt = $dbConn->prepare("SELECT otp FROM otp_expiry WHERE user_id = ? AND otp_expiry_time > NOW()");
+$stmt->execute([$user_id]);
+$hashedOtp = $stmt->fetchColumn();
+
+if (password_verify($otp, $hashedOtp)) {
+    if ($newPassword === $confirmPassword) {
+        $newPasswordHashed = password_hash($newPassword, PASSWORD_DEFAULT);
+        $stmt = $dbConn->prepare("UPDATE users SET password = ? WHERE id = ?");
+        $stmt->execute([$newPasswordHashed, $user_id]);
+        echo json_encode(
+            array(
+                "message" => "Mật khẩu đã được thay đổi"
+            )
+        );
+        // Delete OTP from database
+        $stmt = $dbConn->prepare("DELETE FROM otp_expiry WHERE user_id = ?");
+        $stmt->execute([$user_id]);
+    } else {
+        echo json_encode(
+            array(
+                "message" => "Mật khẩu không khớp"
+            )
+        );
+    }
+} else {
+    echo json_encode(
+        array(
+            "message" => "Mã OTP không hợp lệ hoặc đã hết hạn"
+        )
+    );
+}
+
+
+?>
--- forgot-password.php
+++ forgot-password.php
@@ -5,52 +5,32 @@ header("Access-Control-Allow-Methods: POST"); // You're handling a POST request,
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
-require 'vendor/autoload.php';
 include_once 'connection.php';
-require 'send-otp.php';
+include_once 'function/send-otp.php';
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
-    $newPassword = $_POST['newPassword'];
-    if (!filter_var($email, FILTER_VALIDATE_EMAIL) || empty($newPassword)) {
-        echo json_encode(['error' => 'Invalid email or password.']);
-        exit;
-    }
-
-    $hashedPassword = password_hash($newPassword, PASSWORD_DEFAULT);
-
-    if ($stmt = $mysqli->prepare("SELECT * FROM users WHERE email = ?")) {
-        $stmt->bind_param("s", $email);
-        $stmt->execute();
-        $result = $stmt->get_result();
-
-        if ($result->num_rows > 0) {
-            $row = $result->fetch_array();
-            $user_id = $row['id'];
-            $otp = generateOTP();
-            $otp_expiry_time = date("Y-m-d H:i:s", strtotime("+1 hour"));
-
-            if ($insert_stmt = $mysqli->prepare("INSERT INTO otp_expiry(otp, user_id, otp_expiry_time) VALUES(?, ?, ?)")) {
-                $insert_stmt->bind_param("sis", $otp, $user_id, $otp_expiry_time);
-                $insert_stmt->execute();
-                if ($insert_stmt->affected_rows > 0) {
-                    echo json_encode(['success' => 'OTP generated and sent.']);
-                } else {
-                    echo json_encode(['error' => 'Failed to insert OTP.']);
-                }
-            } else {
-                echo json_encode(['error' => 'Database error.']);
-            }
-        } else {
-            echo json_encode(['error' => 'This email does not exist in our database.']);
-        }
-
-        $stmt->close();
-    } else {
-        echo json_encode(['error' => 'Database error.']);
-    }
+$data = json_decode(file_get_contents("php://input"), true);
+$email = $data['email'];
+$query = "SELECT * FROM users WHERE email = '$email'";
+$stmt = $dbConn->prepare($query);
+$stmt->execute();
+$user = $stmt->fetch(PDO::FETCH_ASSOC);
+if ($user) {
+    $otp = sendOTP($email);
+    //hash the otp
+    $otpHashed = password_hash($otp, PASSWORD_DEFAULT);
+    $query = "INSERT INTO otp_expiry (otp, user_id, otp_expiry_time) VALUES ('$otpHashed', '$user[id]', now()+INTERVAL 60 MINUTE)";
+    $stmt = $dbConn->prepare($query);
+    $stmt->execute();
+    echo json_encode(
+        array(
+            "message" => "Mã OTP đã được gửi tới email của bạn"
+        )
+    );
 } else {
-    echo json_encode(['error' => 'Invalid request method.']);
+    echo json_encode(
+        array(
+            "message" => "Email không tồn tại"
+        )
+    );
 }
-
 ?>
