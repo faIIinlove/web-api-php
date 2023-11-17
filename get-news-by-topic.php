8d11697447a1cd74b841718be65380ec3445681e
faIIinlove <phucnamvan@gmail.com>
2023-11-17 20:16:28 +0700

    Fix headers and add error handling for news retrieval

--- get-news-by-topic.php
+++ get-news-by-topic.php
@@ -1,12 +1,12 @@
 <?php
- header("Content-Type: application/json; charset=UTF-8");
- header("Access-Control-Allow-Methods: GET");
- header("Access-Control-Max-Age: 3600");
- header("Access-Control-Allow-Origin: *");
- header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: GET");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+include_once 'connection.php';
 
- // lấy danh sách news theo topic id từ database
-    include_once 'connection.php';
+try {
     $topic_id = $_GET["topic_id"];
     $query = "SELECT * FROM news WHERE topic_id = $topic_id";
     $stmt = $dbConn->prepare($query);
@@ -16,6 +16,15 @@
         array(
             "data" => $news
         )
-    )
+    );
+} catch (Exception $e) {
+    echo json_encode(
+        array(
+            http_response_code(500),
+            "message" => "No news found."
+        )
+    );
+}
 
- ?>
+
+?>
